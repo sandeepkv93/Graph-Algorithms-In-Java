8c11f8efe5a2411083fa2a1e900600f0efd93092
Sandeep Vishnu <savishnu@microsoft.com>
2022-10-10 01:23:30 -0700

    Optimized Union Find using Path Compression and Union By Rank

---
+++ src/OptimizedUnionFindClient.java
@@ -0,0 +1,69 @@
+class OptimizedUnionFind {
+    // Array to store the parent of each node
+    private final int[] root;
+    // Array to store the rank of each node
+    private final int[] rank;
+
+    // O(n)
+    // Initialize the root array to itself so that root of each node is itself
+    // Initialize the rank array to 1 so that rank of each node is 1 initially
+    public OptimizedUnionFind(int size) {
+        root = new int[size];
+        rank = new int[size];
+        for (int i = 0; i < size; i++) {
+            root[i] = i;
+            rank[i] = 1; // The initial "rank" of each vertex is 1, because each of them is
+            // a standalone vertex with no connection to other vertices.
+        }
+    }
+
+    // The find function here is the same as that in the disjoint set with path compression.
+    // O(α(N)) ~= O(1)
+    public int find(int x) {
+        if (x == root[x]) {
+            return x;
+        }
+        return root[x] = find(root[x]);
+    }
+
+    // The union function with union by rank
+    // O(α(N)) ~= O(1)
+    public void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            if (rank[rootX] > rank[rootY]) {
+                root[rootY] = rootX;
+            } else if (rank[rootX] < rank[rootY]) {
+                root[rootX] = rootY;
+            } else {
+                root[rootY] = rootX;
+                rank[rootX] += 1;
+            }
+        }
+    }
+
+    // O(α(N)) ~= O(1)
+    public boolean connected(int x, int y) {
+        return find(x) == find(y);
+    }
+}
+
+public class OptimizedUnionFindClient {
+    public static void main(String[] args) throws Exception {
+        OptimizedUnionFind unionFind = new OptimizedUnionFind(10);
+        // 1-2-5-6-7 3-8-9 4
+        unionFind.union(1, 2);
+        unionFind.union(2, 5);
+        unionFind.union(5, 6);
+        unionFind.union(6, 7);
+        unionFind.union(3, 8);
+        unionFind.union(8, 9);
+        System.out.println(unionFind.connected(1, 5)); // true
+        System.out.println(unionFind.connected(5, 7)); // true
+        System.out.println(unionFind.connected(4, 9)); // false
+        // 1-2-5-6-7 3-8-9-4
+        unionFind.union(9, 4);
+        System.out.println(unionFind.connected(4, 9)); // true
+    }
+}
