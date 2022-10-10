66b10094afa7181f7984e0571270f66059b2cc03
Sandeep Vishnu <savishnu@microsoft.com>
2022-10-10 14:48:34 -0700

    BFS Iterative Adjacency List

--- src/BFSIterativeAdjListClient.java
+++ src/BFSIterativeAdjListClient.java
@@ -1,18 +1,21 @@
 import java.util.LinkedList;
 import java.util.Queue;
 
-class BFSIterativeAdjMatrix {
+class BFSIterativeAdjList {
     int v;
-    int[][] adjMatrix;
+    LinkedList<Integer>[] adjList;
 
-    public BFSIterativeAdjMatrix(int v) {
+    public BFSIterativeAdjList(int v) {
         this.v = v;
-        adjMatrix = new int[v][v];
+        adjList = new LinkedList[v];
+        for (int i = 0; i < v; i++) {
+            adjList[i] = new LinkedList<>();
+        }
     }
 
     public void addEdge(int src, int dest) {
-        adjMatrix[src][dest] = 1;
-        adjMatrix[dest][src] = 1;
+        adjList[src].add(dest);
+        adjList[dest].add(src);
     }
 
     public void populateGraph(int[][] edges) {
@@ -37,19 +40,19 @@ class BFSIterativeAdjMatrix {
         while(!queue.isEmpty()) {
             int node = queue.poll();
             System.out.print(node + " ");
-            for (int i = 0; i < v; ++i) {
-                if (adjMatrix[node][i] == 1 && !visited[i]) {
-                    queue.offer(i);
-                    visited[i] = true;
+            for (int neighbor : adjList[node]) {
+                if (!visited[neighbor]) {
+                    queue.offer(neighbor);
+                    visited[neighbor] = true;
                 }
             }
         }
     }
 }
 
-public class BFSIterativeAdjMatrixClient {
+public class BFSIterativeAdjListClient {
     public static void main(String[] args) {
-        BFSIterativeAdjMatrix graph = new BFSIterativeAdjMatrix(10);
+        BFSIterativeAdjList graph = new BFSIterativeAdjList(10);
         int[][] edges = {{0, 1}, {0, 2}, {1, 3}, {1, 4}, {4, 5}, {2, 5}, {2, 6}, {2, 7}, {8, 9}};
         graph.populateGraph(edges);
         graph.bfs(); // 0 1 2 3 4 5 6 7 8 9
