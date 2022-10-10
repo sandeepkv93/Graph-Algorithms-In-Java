ba862250214bfdb8d0a167507c58dfe2d8b3ba7c
Sandeep Vishnu <savishnu@microsoft.com>
2022-10-10 00:44:07 -0700

    Adding Comments to QuickFind.java

--- src/QuickFindClient.java
+++ src/QuickFindClient.java
@@ -1,6 +1,8 @@
 class QuickFind {
+    // Array to maintain the root of each nodes
     private final int[] root;
 
+    // Initialize the root array to itself so that root of each node is itself
     public QuickFind(int size) {
         root = new int[size];
         for (int i = 0; i < size; i++) {
@@ -8,10 +10,15 @@ class QuickFind {
         }
     }
 
+    // O(1)
+    // Returns the root of the given element from the root array
     public int find(int x) {
         return root[x];
     }
 
+    // O(n)
+    // Find the root of x and y
+    // If they are not same, then set the root of all the elements for which the root is y to root of x
     public void union(int x, int y) {
         int rootX = find(x);
         int rootY = find(y);
@@ -24,6 +31,8 @@ class QuickFind {
         }
     }
 
+    // O(1)
+    // Call find on x and y, and check if they are same
     public boolean connected(int x, int y) {
         return find(x) == find(y);
     }
