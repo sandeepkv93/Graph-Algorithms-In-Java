9fe40d343f97c94bd81da4d331db6cca14fd6845
Sandeep Vishnu <savishnu@microsoft.com>
2022-10-10 00:29:24 -0700

    Quick Find

---
+++ src/QuickFindClient.java
@@ -0,0 +1,49 @@
+class QuickFind {
+    private final int[] root;
+
+    public QuickFind(int size) {
+        root = new int[size];
+        for (int i = 0; i < size; i++) {
+            root[i] = i;
+        }
+    }
+
+    public int find(int x) {
+        return root[x];
+    }
+
+    public void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            for (int i = 0; i < root.length; i++) {
+                if (root[i] == rootY) {
+                    root[i] = rootX;
+                }
+            }
+        }
+    }
+
+    public boolean connected(int x, int y) {
+        return find(x) == find(y);
+    }
+}
+
+public class QuickFindClient {
+    public static void main(String[] args) {
+        QuickFind quickFind = new QuickFind(10);
+        // 1-2-5-6-7 3-8-9 4
+        quickFind.union(1, 2);
+        quickFind.union(2, 5);
+        quickFind.union(5, 6);
+        quickFind.union(6, 7);
+        quickFind.union(3, 8);
+        quickFind.union(8, 9);
+        System.out.println(quickFind.connected(1, 5)); // true
+        System.out.println(quickFind.connected(5, 7)); // true
+        System.out.println(quickFind.connected(4, 9)); // false
+        // 1-2-5-6-7 3-8-9-4
+        quickFind.union(9, 4);
+        System.out.println(quickFind.connected(4, 9)); // true
+    }
+}
